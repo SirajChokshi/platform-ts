038c66c6373222dbc935ecfcfd2e4dfe231ce75a
Siraj <19193347+SirajChokshi@users.noreply.github.com>
2023-03-04 12:46:58 -0800

    revert outputFileTracingRoot

--- website/next.config.js
+++ website/next.config.js
@@ -1,12 +1,6 @@
-const { join } = require("path");
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: "standalone",
-  experimental: {
-    outputFileTracingRoot: join(__dirname, "../../"),
-  },
 };
 
 module.exports = nextConfig;
